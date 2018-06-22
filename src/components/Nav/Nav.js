504c3252a86967bb6267928a7d6f036751345a56
Alex Gjorvad <alexgjorvad@gmail.com>
2018-06-22 14:37:53 -0500

    nav bar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,38 +1,162 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import MenuItem from '@material-ui/core/MenuItem';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import {Link} from 'react-router-dom';
 
-const styles = {
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+ 
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 20,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+  },
   list: {
-    width: '350'
-  }
-}
-const Nav = () => (
-  <div className="navbar">
-    <div>
-      <ul>
-        <li>
-          <Link to="/user">
-            User Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/search">
+    textAlign: 'center',
+  },
+  'contentShift-left': {
+        justifyContent: 'center',
+      },
+})
+
+class Nav extends React.Component {
+  state = {
+    open: true,
+    anchor: 'left',
+  };
+
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleDrawerClose = () => {
+    this.setState({ open: false });
+  };
+
+  handleChangeAnchor = event => {
+    this.setState({
+      anchor: event.target.value,
+    });
+  };
+
+  render() {
+    const { classes, theme } = this.props;
+    const { anchor, open } = this.state;
+
+    const drawer = (
+      <Drawer
+        variant="persistent"
+        anchor={anchor}
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div>
+          <IconButton onClick={this.handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </div>
+        <div className={classes.list}>
+        <Divider />
+        <List>  
+              <Link to="/user">
+              User Home
+            </Link>
+            </List>
+            <Divider />
+            <List>
+            <Link to="/search">
             Search Page
           </Link>
-        </li>
-        <li>
+          </List>
+          <Divider />
+          <List>
           <Link to="/summary">
             Summary Page
           </Link>
-        </li>
-        <li>
+          </List>
+          <Divider />
+          <List>
           <Link to="/editpartner">
             Partner Page
           </Link>
-        </li>
-      </ul>
-    </div>
-  </div>
-);
+            </List>
+            </div>
+      </Drawer>
+    );
 
-export default Nav;
+    let before = null;
+    let after = null;
+
+    if (anchor === 'left') {
+      before = drawer;
+    } else {
+      after = drawer;
+    }
+
+    return (
+      <div className={classes.root}>
+            <Toolbar disableGutters={!open}>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={this.handleDrawerOpen}
+                className={classNames(classes.menuButton, open && classes.hide)}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="title" color="inherit" noWrap>
+              </Typography>
+            </Toolbar>
+          {before}
+          <main
+            className={classNames(classes.content, classes[`content-${anchor}`], {
+              [classes.contentShift]: open,
+              [classes[`contentShift-${anchor}`]]: open,
+            })}>
+          </main>
+          {after}
+      </div>
+    );
+  }
+}
+
+Nav.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(Nav);
+   
+
