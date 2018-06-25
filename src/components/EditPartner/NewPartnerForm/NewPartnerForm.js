3eef1c2140440e4f37153ea21af8a9b7dce50c70
Greg Orvik <gvorvik@gmail.com>
2018-06-25 15:53:51 -0500

    set up form fields and submit button for new partner form

--- src/components/EditPartner/NewPartnerForm/NewPartnerForm.js
+++ src/components/EditPartner/NewPartnerForm/NewPartnerForm.js
@@ -6,6 +6,12 @@ class NewPartnerForm extends Component {
 
         this.state = {
             orgName: '',
+            orgAddress: '',
+            orgWebsite: '',
+            orgPhone: '',
+            directorFirst: '',
+            directorLast: '',
+            businessType: '',
         }
     }
 
@@ -15,27 +21,46 @@ class NewPartnerForm extends Component {
         });
     }
 
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            orgName: '',
+            orgAddress: '',
+            orgWebsite: '',
+            orgPhone: '',
+            directorFirst: '',
+            directorLast: '',
+            businessType: '',
+        });
+    }
+
     render() {
 
-        return <form>
+        return <form onSubmit={this.handleSubmit}>
             <h2>Register New Partner</h2>
             <div>
                 <label htmlFor="orgName">Organization Name</label>
                 <input onChange={this.handleChange} type="text" name="orgName" id="orgName"value={this.state.orgName}/>
             </div>
-            
-            <label htmlFor="orgName">Address</label>
-            <input onChange={this.handleChange} type="text" name="orgName" id="orgName"value={this.state.orgName}/>
-            <label htmlFor="orgName">Website</label>
-            <input onChange={this.handleChange} type="text" name="orgName" id="orgName"value={this.state.orgName}/>
-            <label htmlFor="orgName">Phone Number</label>
-            <input onChange={this.handleChange} type="text" name="orgName" id="orgName"value={this.state.orgName}/>
-            <label htmlFor="orgName">Director First Name</label>
-            <input onChange={this.handleChange} type="text" name="orgName" id="orgName"value={this.state.orgName}/>
-            <label htmlFor="orgName">Director Last Name</label>
-            <input onChange={this.handleChange} type="text" name="orgName" id="orgName"value={this.state.orgName}/>
-            <label htmlFor="orgName">Business Type</label>
-            <input onChange={this.handleChange} type="text" name="orgName" id="orgName"value={this.state.orgName}/>
+            <div>
+                <label htmlFor="orgAddress">Address</label>
+                <input onChange={this.handleChange} type="text" name="orgAddress" id="orgAddress"value={this.state.orgAddress}/>
+            </div>
+            <div>
+                <label htmlFor="orgWebsite">Website</label>
+                <input onChange={this.handleChange} type="text" name="orgWebsite" id="orgWebsite"value={this.state.orgWebsite}/>
+                <label htmlFor="orgPhone">Phone Number</label>
+                <input onChange={this.handleChange} type="text" name="orgPhone" id="orgPhone"value={this.state.orgPhone}/>
+            </div>
+            <div>
+                <label htmlFor="directorFirst">Director First Name</label>
+                <input onChange={this.handleChange} type="text" name="directorFirst" id="directorFirst"value={this.state.directorFirst}/>
+                <label htmlFor="directorLast">Director Last Name</label>
+                <input onChange={this.handleChange} type="text" name="directorLast" id="directorLast"value={this.state.directorLast}/>
+                <label htmlFor="businessType">Business Type</label>
+                <input onChange={this.handleChange} type="text" name="businessType" id="businessType"value={this.state.businessType}/>
+            </div>
+            <input type="submit"/>
         </form>
     }
 }
