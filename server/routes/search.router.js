784d3bc2e36914f6ea9cad74fe678559672e3c63
Liz Goff <livingearthcreations@gmail.com>
2018-06-26 11:28:19 -0500

    serach working on INTEGERS and VARCHAR

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -7,7 +7,22 @@ const router = express.Router();
 router.get('/', (req, res) => {
   if (req.isAuthenticated()) {
     const queryText = `SELECT * FROM "person"
-                      WHERE "gender"::text ILIKE $1::text;`; // TO DO Liz needs to look into only returning first 50, next step is pagination
+                      WHERE "partner_id"::text ILIKE $1::text
+                      OR "formatted_id"::text ILIKE $1::text
+                      OR "gender"::text ILIKE $1::text
+                      OR "year_of_birth"::text ILIKE $1::text
+                      OR "person_of_color"::text ILIKE $1::text
+                      OR "education_level"::text ILIKE $1::text
+                      OR "city_of_residence"::text ILIKE $1::text
+                      OR "scholarship_recipient"::text ILIKE $1::text
+                      OR "previous_job_experience"::text ILIKE $1::text
+                      OR "pre_training_wage"::text ILIKE $1::text
+                      OR "training_start_date"::text ILIKE $1::text
+                      OR "training_status"::text ILIKE $1::text
+                      OR "training_end_date"::text ILIKE $1::text
+                      OR "training_type"::text ILIKE $1::text
+                      OR "classroom_or_online"::text ILIKE $1::text
+                      OR "exit_status"::text ILIKE $1::text`; // TO DO only return first 50, next step is pagination
     pool.query(queryText, [req.query.search])
       .then((result) => { res.send(result.rows); })
       .catch((err) => {
@@ -30,9 +45,9 @@ router.get('/', (req, res) => {
 //                       SET "gender" = $1 
 //                       WHERE "id" = $2`;
 //     const queryValues = [
-//       newInfo.gender, ???
-//       req.params.id, ???
-//       req.user.id, ???
+//       newInfo.gender,
+//       req.params.id,
+//       req.user.id,
 //     ];
 //     pool.query(queryText, queryValues)
 //       .then(() => { res.sendStatus(200); })
