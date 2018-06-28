464b90c7b3ae6c59d6197bef3cb4f384b2469eda
Michele <micheleannedexter@gmail.com>
2018-06-28 08:16:14 -0500

    fix the json to csv converter

--- src/components/JsonArrayToCsv/JsonArrayToCsv.js
+++ src/components/JsonArrayToCsv/JsonArrayToCsv.js
@@ -33,8 +33,6 @@ const exportCsv = encodedUri => {
   let link = document.createElement('a');
   link.setAttribute('href', encodedUri);
   link.setAttribute('download', 'data.csv');
-  link.innerHTML = 'Download CSV Spreadsheet';
-  document.body.appendChild(link);
   link.click();
 }
 
